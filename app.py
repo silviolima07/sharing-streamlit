b835e602df6cb47c98f12dcf9644c2a9215c4896
Silvio <silviolima@gmail.com>
2020-10-20 14:37:23 -0300

    First commit

--- app.py
+++ app.py
@@ -19,6 +19,15 @@ def main():
     image1 = Image.open("aguia2.jpg")
     st.image(image1,caption="",use_column_width=True)
 
+    image2 = Image.open("aguia3.jpg")
+    st.image(image2,caption="",use_column_width=True)
+
+    image3 = Image.open("aguia4.jpg")
+    st.image(image3,caption="",use_column_width=True)
+
+    image4 = Image.open("aguia8.jpg")
+    st.image(image4,caption="",use_column_width=True)
+
 
 if __name__ == '__main__':
     main()
