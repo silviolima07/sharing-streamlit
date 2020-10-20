c087d83b153010cb6e5fb0a1c3f5f9ceddbcc51a
Silvio <silviolima@gmail.com>
2020-10-20 13:58:34 -0300

    First commit

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ def main():
     
  
     html_page = """
-    <div style="background-color:red;padding=10px">
-        <p style='color:white;text-align:center;font-size:20px;font-weight:bold'>Sharing</p>
+    <div style="background-color:red;padding=50px">
+        <p style='color:white;text-align:center;font-size:30px;font-weight:bold'>Streamlit Sharing</p>
     </div>
               """
     st.markdown(html_page, unsafe_allow_html=True)    
